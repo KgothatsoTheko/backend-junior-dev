2a15d0137683ec6bf51e91e7b379d0eab43d0a31
KgothatsoTheko <kgothatsotheko7@gmail.com>
2025-06-14 14:00:16 +0200

    code upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const { data } = req.body;
+  if (!data || typeof data !== 'string') {
+    return res.status(400).json({ error: 'Invalid input' });
+  }
+
+  const sorted = data.split('').sort();
+  res.json({ word: sorted });
+});
+
+module.exports = app;
